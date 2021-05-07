0e89515883fa1731d08546fee07c6ad9f9e9f184
u10606240 <u10606240@ms.ttu.edu.tw>
2021-05-07 22:57:19 +0800

    0507-1

--- hw3/keyframes.js
+++ hw3/keyframes.js
@@ -32,16 +32,16 @@ var keysStand = [
 var TStand = 0.5; // any small amount
 
 //// stand-to-walk //////////
-/*
+
 var poseS2W0 = { // stand
   lThigh: 0,
   rThigh: 0
 }
-*/
+
 var poseS2W0 = poseStand1; // end of stand
 var poseS2W1 = {
   lThigh: -Math.PI / 6,
-  rThigh: Math.PI/40 // right leg stays still
+  rThigh: 0 // right leg stays still
 }
 var poseS2W2 = poseWalk0; // beginning of walk
 var keysS2W = [
@@ -60,10 +60,10 @@ var poseW2S0 = { // walk
 */
 var poseW2S0 = poseWalk1; // end of walk
 var poseW2S1 = {
-  lThigh: -Math.PI / 6,
-  rThigh: Math.PI/40 // right leg stays still
+  lThigh: 0,
+  rThigh: -Math.PI / 6 // right leg stays still
 }
-var poseW2S2 = poseStand0; // beginning of stand
+var poseW2S2 = poseWalk0; // beginning of walk
 var keysW2S = [
   [0, poseW2S0],
   [0.5, poseW2S1],
@@ -129,7 +129,51 @@ function keyframeWalk(t, T) { // walk; periodic
   return intKey;
 }
 
-function keyframeW2S(t, T) {  // non-periodic
+function keyframeW2S(t, T, lleg, rleg) {  // non-periodic
+  /*var cases;
+  console.log(cases);
+  
+  if(cases == 1){
+ console.log(1);
+ intKey = keyframeWalk(clock.getElapsedTime(), TWalk);
+ if(intKey[0] - (Math.PI / 6) < 0.01 && intKey[1] + 1 * (Math.PI / 6) < 0.01) cases = 0;
+  }
+  else{
+  if (t - ts >= T) { // end of walk
+    console.log('switch to stand');
+    ts = t; // reset ts to start of walk
+    state = 'Stand';
+    // end of S2W: return last frame
+ cases = 0;
+    return [0, 0];
+  }
+ var last_walk_pose = {
+ lThigh: lleg,
+ rThigh: rleg
+ }
+  // non-periodic stand-to-walk animation
+  let keys = [
+  [0, poseS2W2],
+  [0.5, poseS2W1],
+  [1, poseS2W0],
+];
+  var s = ((t - ts) % T) / T;
+
+  for (var i = 1; i < keys.length; i++) {
+    if (keys[i][0] > s) break;
+  }
+  // take i-1
+  var ii = i - 1;
+  var a = (s - keys[ii][0]) / (keys[ii + 1][0] - keys[ii][0]);
+  intKey = [keys[ii][1].lThigh * (1 - a) + keys[ii + 1][1].lThigh * a,
+    keys[ii][1].rThigh * (1 - a) + keys[ii + 1][1].rThigh * a
+  ];
+  }
+  return intKey;*/
+  /*var status = 1;
+  if(status == 1){
+	
+  }*/
   if (t - ts > T) { // end of walk
     ts = t; // reset ts to walk of stand
     state = 'Stand';
@@ -138,6 +182,11 @@ function keyframeW2S(t, T) {  // non-periodic
   }
 
   // non-periodic walk-to-stand animation
+  /*let keys = [
+  [0,poseS2W1],
+  [0.5,poseS2W1],
+  [1,poseS2W0]
+  ];*/
   let keys = keysW2S;
   var s = (t - ts) / T;
 
